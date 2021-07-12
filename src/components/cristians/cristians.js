c955d9d6cea1b869e77b7330d365508d7c8d1831
VladimirLashmanov <vov7las@gmail.com>
2021-07-12 20:52:46 +0300

    lesson react 1 исправил cristians

---
+++ src/components/cristians/cristians.js
@@ -0,0 +1,21 @@
+export default function Cristians() {
+    return (
+        <div>
+
+
+
+            <h2>Lusy </h2>
+
+            <img src={'https://i.pinimg.com/originals/3c/8f/9a/3c8f9ad07c5f305354cdf087cf1d580c.jpg'} alt=""/>
+
+                <div>lorem</div>
+
+
+        </div>
+    );
+
+
+            }
+
+
+
